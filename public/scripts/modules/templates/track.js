34a623cb25da655b512b7fe47c2789f9798c193a
Ben <benvenda@gmail.com>
2017-07-16 22:41:38 -0400

    Sources and Tags are now on separate lines, and the one-shot 'Play Once' button is now phanary yellow.

--- public/scripts/modules/templates/track.js
+++ public/scripts/modules/templates/track.js
@@ -5,7 +5,7 @@ templates['track.hbs'] = template({"1":function(container,depth0,helpers,partial
 
   return "            <span>Source:\r\n                <span class=\"source\">"
     + container.escapeExpression(((helper = (helper = helpers.source || (depth0 != null ? depth0.source : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"source","hash":{},"data":data}) : helper)))
-    + "</span>\r\n            </span>\r\n";
+    + "</span>\r\n            </span>\r\n            <br>\r\n";
 },"3":function(container,depth0,helpers,partials,data) {
     var stack1;
 
